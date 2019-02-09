0c1f8c8df65a21dcc3b2ed989f0f406b4944bbf1
Temitayo Fagbami <temitayo.fagbami@gmail.com>
2019-02-09 14:52:10 -0800

    building token api-added identityserver4 nuget packet -OpenID Connect and OAuth2.0

---
+++ TokenServiceAPI/Models/ApplicationUser.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Identity;
+
+namespace TokenServiceAPI.Models
+{
+    //add profile data for application user by adding properties
+    public class ApplicationUser : IdentityUser
+    {
+    }
+}
